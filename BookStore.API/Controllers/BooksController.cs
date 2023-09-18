654c4417566d0c039fd547ca7e930e20346c3ea9
Fagun <mdfagun1357@gmail.com>
2023-09-18 10:37:02 +0600

    first commit

---
+++ BookStore.API/Controllers/BooksController.cs
@@ -0,0 +1,27 @@
+﻿using BookStore.API.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet("")]
+
+        public async Task<IActionResult> GetAllBooks()
+        {
+            var books = await _bookRepository.GetAllBooksAsync();
+            return Ok(books);
+        }
+    }
+}
---
+++ BookStore.API/Repository/BookRepository.cs
@@ -0,0 +1,31 @@
+﻿using BookStore.API.Data;
+using BookStore.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.API.Repository
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookStoreContext _context;
+
+        public BookRepository(BookStoreContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<BookModel>> GetAllBooksAsync()
+        {
+            var records =await _context.Books.Select(x=>new BookModel()
+            {
+                Id= x.Id,
+                Title = x.Title,
+                Description= x.Description
+
+            }).ToListAsync();
+
+            return records;
+        }
+    }
+}
